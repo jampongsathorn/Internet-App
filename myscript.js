ad5e3b985430e729cb41d84328e9d130ee7da15b
JamPongsathorn <85749998+jampongsathorn@users.noreply.github.com>
2022-08-30 23:35:18 +0700

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,14 @@
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "150px";
+    document.getElementById("main").style.marginLeft = "150px";
+}
+    
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0px";
+    document.getElementById("main").style.marginLeft= "0px";
+}
+
+
+
+  
